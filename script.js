c023386bf5d832e3ef6be9cae7b974e69d1d34e2
AAAAAaaaa <anviddipu@gmail.com>
2025-06-03 23:52:44 -0400

    int

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const slider = document.getElementById('slider');
         });
 
 
-a = document.getElementById("input1")
-b = document.getElementById("input2")
+const a = document.getElementById("input1")
+const b = document.getElementById("input2")
 document.getElementById("b1").addEventListener('click', function(){
     output1.textContent = "Number here:" + (a.value + b.value)
 })
